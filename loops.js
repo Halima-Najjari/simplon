57b16865038433bb55a3c43645ffbdbdf82db7fc
haliam najjari <halimanajjari072@gmail.com>
2024-11-21 16:33:51 +0100

    first commit

---
+++ loops.js
@@ -0,0 +1,12 @@
+//qestion1
+console.log(typeof("hello word "))
+//qestion2
+let x=10
+x=20
+console.log(x)
+//qestion3
+const fruit={
+    pomme:"red",
+    ananass:"ver",
+}
+console.log(fruit.pomme)
